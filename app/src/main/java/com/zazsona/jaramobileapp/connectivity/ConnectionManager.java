7a85d68aa8eba263bebafcd1bd9e1d28467b6df5
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-11-25 17:57:18 +0000

    Add sendPing and sendReportRequest methods to ConnectionManager.

--- app/src/main/java/com/zazsona/jaramobileapp/connectivity/ConnectionManager.java
+++ app/src/main/java/com/zazsona/jaramobileapp/connectivity/ConnectionManager.java
@@ -1,20 +1,23 @@
 package com.zazsona.jaramobileapp.connectivity;
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.zazsona.jaramobileapp.connectivity.requests.ReportRequest;
+import com.zazsona.jaramobileapp.connectivity.responses.ReportResponse;
 import com.zazsona.jaramobileapp.settings.Settings;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class ConnectionManager implements Serializable
+public class ConnectionManager
 {
     private static ConnectionManager connectionManager;
 
+    private Gson gson;
     private Context context;
     private Socket socket;
     private ObjectInputStream ois;
@@ -23,7 +26,7 @@ public class ConnectionManager implements Serializable
     private ConnectionManager(Context context)
     {
         this.context = context;
-        socket = new Socket();
+        this.gson = new Gson();
     }
 
     public static ConnectionManager getInstance(Context context)
@@ -35,9 +38,46 @@ public class ConnectionManager implements Serializable
         return connectionManager;
     }
 
-    public void connect() throws IOException
+    private void connect() throws IOException
     {
-        socket.connect(new InetSocketAddress(InetAddress.getByName(Settings.getInstance(context).getIp()), Integer.parseInt(Settings.getInstance(context).getPort())), 5000);
+        socket = new Socket();
+        socket.connect(new InetSocketAddress(
+                InetAddress.getByName(Settings.getInstance(context).getIp()),
+                Integer.parseInt(Settings.getInstance(context).getPort())),
+                       5000);
+        ois = new ObjectInputStream(socket.getInputStream());
+        oos = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    private void disconnect() throws IOException
+    {
+        socket.close();
+        ois.close();
+        oos.close();
+        socket = null;
+        ois = null;
+        oos = null;
+    }
+
+    public boolean sendPing() throws IOException
+    {
+        connect();
+        boolean success = isConnected();
+        disconnect();
+        return success;
+    }
+
+    public ReportResponse sendReportRequest() throws IOException, ClassNotFoundException
+    {
+        connect();
+        ReportRequest reportRequest = new ReportRequest();
+        String requestJson = gson.toJson(reportRequest);
+        oos.writeObject(requestJson);
+        oos.flush();
+        String responseJson = (String) ois.readObject();
+        disconnect();
+        ReportResponse reportResponse = gson.fromJson(responseJson, ReportResponse.class);
+        return reportResponse;
     }
 
     public boolean isConnected()
