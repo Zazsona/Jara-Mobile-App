df4f8d97b430923dd4cbcb4a4cd792eb5ea2558c
Zazsona <40435178+Zazsona@users.noreply.github.com>
2019-11-25 18:03:46 +0000

    Add functionality to regularly request data and print it on the UI.

--- app/src/main/java/com/zazsona/jaramobileapp/overview/OverviewFragment.java
+++ app/src/main/java/com/zazsona/jaramobileapp/overview/OverviewFragment.java
@@ -1,15 +1,13 @@
 package com.zazsona.jaramobileapp.overview;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
+import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -17,16 +15,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.zazsona.jaramobileapp.R;
 import com.zazsona.jaramobileapp.connectivity.ConnectionManager;
-import com.zazsona.jaramobileapp.settings.Settings;
-import com.zazsona.jaramobileapp.settings.SettingsActivity;
+import com.zazsona.jaramobileapp.connectivity.responses.ReportResponse;
 
 import java.io.IOException;
 
 public class OverviewFragment extends Fragment
 {
+    public static final String STATUS_TAG = OverviewFragment.class.getCanonicalName()+".status";
+    private ReportResponse status;
+
+    private BroadcastReceiver minuteBroadcastReceiver;
+
     private OnOverviewFragmentInteractionListener mListener;
 
     public static OverviewFragment newInstance()
@@ -38,38 +42,25 @@ public class OverviewFragment extends Fragment
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_overview, container, false);
         final ImageView onlineStatus = view.findViewById(R.id.imageOnlineStatus);
-        new AsyncTask<String, String, String>()
-        {
-
-            @Override
-            protected String doInBackground(String... strings)
-            {
-                try{Thread.sleep(1000);}catch(InterruptedException e){e.printStackTrace();};
-                try{ConnectionManager.getInstance(getContext()).connect();}catch(IOException e){e.printStackTrace();};
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(String s)
-            {
-                super.onPostExecute(s);
-                if (ConnectionManager.getInstance(getContext()).isConnected())
-                {
-                    onlineStatus.setImageResource(R.drawable.ic_cloud_black_24dp);
-                }
-                else
-                {
-                    onlineStatus.setImageResource(R.drawable.ic_cloud_off_black_24dp);
-                }
-            }
-        }.execute();
         Button settingsButton = view.findViewById(R.id.settingsButton);
+
+        if (savedInstanceState != null)
+        {
+            status = (ReportResponse) savedInstanceState.get(STATUS_TAG);
+            if (status == null)
+                new ConnectionTask(onlineStatus).execute();
+            else
+                Picasso.get().load(status.getProfileImageURL()).into(onlineStatus);
+        }
+        else
+        {
+            new ConnectionTask(onlineStatus).execute();
+        }
+
         settingsButton.setOnClickListener(new View.OnClickListener()
         {
 
@@ -79,7 +70,36 @@ public class OverviewFragment extends Fragment
                 mListener.onSettingsButtonPressed(view);
             }
         });
+        minuteBroadcastReceiver = new BroadcastReceiver()
+        {
+            @Override
+            public void onReceive(Context context, Intent intent)
+            {
+                new ConnectionTask(onlineStatus).execute();
+            }
+        };
         return view;
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        getActivity().registerReceiver(minuteBroadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        getActivity().unregisterReceiver(minuteBroadcastReceiver);
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState)
+    {
+        outState.putSerializable(STATUS_TAG, status);
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -100,5 +120,45 @@ public class OverviewFragment extends Fragment
     public interface OnOverviewFragmentInteractionListener
     {
         public void onSettingsButtonPressed(View view);
+    }
+
+    private class ConnectionTask extends AsyncTask<String, String, ReportResponse>
+    {
+        private ImageView avatarView;
+
+        public ConnectionTask(ImageView avatarView)
+        {
+            this.avatarView = avatarView;
+        }
+
+        @Override
+        protected ReportResponse doInBackground(String... strings)
+        {
+            try
+            {
+                status = ConnectionManager.getInstance(getContext()).sendReportRequest();
+                return status;
+            }
+            catch (IOException | ClassNotFoundException e)
+            {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ReportResponse reportResponse)
+        {
+            super.onPostExecute(reportResponse);
+            if (reportResponse != null)
+            {
+                Picasso.get().load(reportResponse.getProfileImageURL()).into(avatarView);
+                Toast.makeText(getContext(), "Commands: "+reportResponse.getCommandUsageForSession(), Toast.LENGTH_LONG).show();
+            }
+            else
+            {
+                avatarView.setImageResource(R.drawable.ic_cloud_off_black_24dp);
+            }
+        }
     }
 }
